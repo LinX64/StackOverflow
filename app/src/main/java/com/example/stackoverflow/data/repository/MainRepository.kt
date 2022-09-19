bc60307f0ffa0773226a28af60456aed6b43cb88
Mohsen <ash.wxrz@hotmail.com>
2022-09-19 11:54:40 +0300

    Added catch in the flow

--- app/src/main/java/com/example/stackoverflow/data/repository/MainRepository.kt
+++ app/src/main/java/com/example/stackoverflow/data/repository/MainRepository.kt
@@ -4,13 +4,14 @@ import com.example.stackoverflow.data.api.ApiService
 import com.example.stackoverflow.util.NetworkResult
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onStart
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(private val apiService: ApiService) {
 
     suspend fun getQuestions() = flow {
+        emit(NetworkResult.Loading(true))
+
         val response = apiService.getQuestions()
         emit(NetworkResult.Success(response.items))
-    }
+    }.catch { e -> emit(NetworkResult.Error(e.message.toString())) }
 }
